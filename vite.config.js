1e8846eb48bf56ff7d14acb64a8129caf6fcdba8
Abdelrehim, Ahmed <ahmed.abdelrehim@partner.bmw.de>
2025-10-12 19:47:48 +0300

    Add Github pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
  
 export default defineConfig({
-  base: '/<repo-name>/', // <-- replace with your repo name
+  base: '/Iso8800-landing/', // must match repo name exactly (case-sensitive)
   plugins: [react()],
 })
