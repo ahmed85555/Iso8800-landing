1f8bec216228da845c7f5cae5d8b36971c127fc8
Abdelrehim, Ahmed <ahmed.abdelrehim@partner.bmw.de>
2025-10-12 19:39:44 +0300

    Add Github pages workflow

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
  
-// IMPORTANT: set the base to your repo name
 export default defineConfig({
-  base: '/iso8800-landing/',  // <-- replace with your repo name
+  base: '/<repo-name>/', // <-- replace with your repo name
   plugins: [react()],
 })
