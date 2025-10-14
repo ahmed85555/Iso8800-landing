2865e9e2ac745a9185ffe3e18bfe4a4c5fc1233f
Ahmed Abdlrehem <ahmedabdlrehem78@gmail.com>
2025-10-15 00:41:03 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
- 
+
 export default defineConfig({
-  base: '/Iso8800-landing/', // must match repo name exactly (case-sensitive)
   plugins: [react()],
+  base: '/Iso8800-landing/',   // your repo name
 })
